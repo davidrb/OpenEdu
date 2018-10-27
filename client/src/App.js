34d937d7de6744867718a89802ed65ad1f398782
David Badke <davidrbadke@gmail.com>
2018-10-26 15:58:01 -1000

    Implement login form with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,83 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux'
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import './App.css'
 
-import './App.css';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { };
+let rootReducer = (state, action) => {
+  switch(action.type) {
+    case "LOGOUT":
+      return { ...state, username: null };
+    case "CHANGE_USER":
+      return { ...state, username: action.user };
+    case "ERROR":
+      return { ...state, error: action.what };
+    case "CLEAR_ERROR":
+      return { ...state, error: null };
+    default:
+      return {};
   }
+}
 
-  authenticate(email, password) {
-    axios.post('http://localhost:3000/user_token',
-              { auth: { email: email, password: password } }
-    ).then(res => {
+function login(email, password) {
+  return dispatch => {
+    return axios.post(
+      'http://localhost:3000/user_token',
+      { auth: { email, password } }
+    )
+    .then(res => {
       axios.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.jwt;
       axios.get('http://localhost:3000/secret')
-           .then(res => this.setState({ username: res.data.username }));
-    });
-  }
+           .then(res => dispatch({ type: "CHANGE_USER", user: res.data.username }));
+      },
+      err => {
+        dispatch({type: "LOGOUT"});
+        dispatch(timedError("login failed", 2000))
+      });
+  };
+}
 
-  componentDidMount() {
-    this.authenticate("bob@gmail.com", "hello");
-  }
+function timedError(what, time) {
+  return dispatch => {
+    dispatch({ type: "ERROR", what });
 
+    return new Promise(resolve => setTimeout(resolve, time))
+          .then(() => dispatch({ type: "CLEAR_ERROR" }));
+  };
+}
+
+let OpenEdu = (props) => {
+  let emailfield;
+  let passwordfield;
+
+  return (
+    <div className="App">
+      <p>{props.username ? "hello, " + props.username : "not logged in"}</p>
+      <form action="#">
+        <input ref={node => (emailfield = node)} />
+        <input ref={node => (passwordfield = node)} />
+        <button onClick={() => props.login(emailfield.value, passwordfield.value)}>Log In</button>
+        <button onClick={() => props.logout()}>Logout</button>
+      </form>
+      { props.error ? <p>{props.error}</p> : null }
+    </div>
+  );
+}
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <p>{ this.state.username || "authenticating..." }</p>
-      </div>
-    );
+    const Main = connect(
+      state => ({ ...state, username: state.username }),
+      dispatch => ({
+        login: (email, password) => dispatch(login(email, password)),
+        logout: () => dispatch({ type: "LOGOUT" })
+      })
+    )(OpenEdu);
+
+    return (<Provider store={store}><Main /></Provider>);
   }
 }
 
