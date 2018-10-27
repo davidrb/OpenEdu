0773ef5da2fead6cd53f622078ffd98bddcf0bc1
David Badke <davidrbadke@gmail.com>
2018-10-26 21:51:31 -1000

    Create Course and Topic models

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181026092948) do
+ActiveRecord::Schema.define(version: 20181027074836) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_topics_on_course_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
